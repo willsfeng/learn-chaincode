8aa53826fa2445c82fa93eaf14f9733ef9d630f3
willsfeng <39547467@qq.com>
2017-03-13 13:29:22 +0800

    Updated for second testing

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/*updated by Wills on 2017-03-13 */
+/*updated by Wills on 2017-03-13 
+add one more comments for testing*/
 package main
 
 import (
